329638d385e6214c93f61fa07fb5e1b0ff2d9fe2
JReynerio <joseph@trademark-dme.com>
2025-09-15 08:58:02 -0600

    Forcing deployment

--- src/main.ts
+++ src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
-  logger.log(`Gateway running on port ${envs.port}`);
+  logger.log(`***** Gateway running on port ${envs.port} *****`);
 }
 bootstrap();
